95af7e2076c90f833ed2d9e295e4db59013a8093
Mateusz Niziołek <mateusz.niziolek@creativestyle.pl>
2020-11-26 12:12:42 +0000

    [GNP 604] sale badge tier price

--- Model/ProductPriceResolver/Configurable.php
+++ Model/ProductPriceResolver/Configurable.php
@@ -9,14 +9,28 @@ class Configurable extends ProductPriceResolver implements ProductPriceResolverI
      */
     protected $getMaxPriceForConfigurableProduct;
 
+    /**
+     * @var \MageSuite\Discount\Model\Command\GetMinSpecialPriceForConfigurableProduct
+     */
+    protected $getMinSpecialPriceForConfigurableProduct;
+
+    /**
+     * @var \MageSuite\Discount\Helper\Configuration
+     */
+    protected $configuration;
+
     public function __construct(
         \MageSuite\Discount\Api\Data\ProductPricesInterfaceFactory $productPricesFactory,
-        \MageSuite\Discount\Model\Command\GetMaxPriceForConfigurableProduct $getMaxPriceForConfigurableProduct
+        \MageSuite\Discount\Model\Command\GetMaxPriceForConfigurableProduct $getMaxPriceForConfigurableProduct,
+        \MageSuite\Discount\Model\Command\GetMinSpecialPriceForConfigurableProduct $getMinSpecialPriceForConfigurableProduct,
+        \MageSuite\Discount\Helper\Configuration $configuration
     ) {
 
-        parent::__construct($productPricesFactory);
+        parent::__construct($productPricesFactory, $configuration);
 
         $this->getMaxPriceForConfigurableProduct = $getMaxPriceForConfigurableProduct;
+        $this->getMinSpecialPriceForConfigurableProduct = $getMinSpecialPriceForConfigurableProduct;
+        $this->configuration = $configuration;
     }
 
     public function isApplicable($productTypeId)
@@ -27,18 +41,27 @@ class Configurable extends ProductPriceResolver implements ProductPriceResolverI
     public function getPrices($product, $finalPrice)
     {
         $regularPrice = $this->getMaxPriceForConfigurableProduct->execute($product);
-
-        if (!$finalPrice) {
-            $finalPrice = $product->hasData('min_price') ?
-                $product->getData('min_price') :
-                $product->getPriceInfo()->getPrice(\Magento\Catalog\Pricing\Price\FinalPrice::PRICE_CODE)->getAmount()->getValue();
-        }
-
         $productPricesContainer = $this->getProductPricesContainer();
 
         $productPricesContainer
-            ->setRegularPrice($regularPrice)
-            ->setFinalPrice($finalPrice);
+            ->setRegularPrice($regularPrice);
+
+        if ($finalPrice) {
+            $productPricesContainer->setFinalPrice($finalPrice);
+            return $productPricesContainer;
+        }
+
+        if ($this->configuration->isSpecialPriceResolverEnabled()) {
+            $specialPriceMinimum = $this->getMinSpecialPriceForConfigurableProduct->execute($product, $regularPrice);
+            $productPricesContainer->setFinalPrice($specialPriceMinimum);
+            return $productPricesContainer;
+        }
+
+        $finalPrice = $product->hasData('min_price') ?
+            $product->getData('min_price') :
+            $product->getPriceInfo()->getPrice(\Magento\Catalog\Pricing\Price\FinalPrice::PRICE_CODE)->getAmount()->getValue();
+
+        $productPricesContainer->setFinalPrice($finalPrice);
 
         return $productPricesContainer;
     }
